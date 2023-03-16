3c8c3fa9f29ff5d1bd90ccf2462234bbcb3105ac
leojkonko <leonardolinomeyer@gmail.com>
2023-03-16 16:57:04 -0300

    cpm - completo

--- CPM-Tuneis/src/Template/Paginas/empresa.view.php
+++ CPM-Tuneis/src/Template/Paginas/empresa.view.php
@@ -1,11 +1,27 @@
 <main id="empresa">
     <section class="empresa py-lg-4 py-2">
+    <div class="row p-0 d-block d-lg-none">
+                <div class="col-12 w-100 p-0">
+                    <div class="swiper consultoria-swiper">
+                        <div class="swiper-wrapper my-lg-4 my-4">
+                            <?php foreach(range(0,9) as $i) : ?>
+                                <div class="swiper-slide">
+                                    <img class="w-100 h-100 object-fit-cover" src="<?= asset('images/backgrounds/img_principal.png') ?>" alt="">
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                </div> 
+            </div>
+        </div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 d-flex align-items-center">
                     <div class="d-flex justify-content-center">
-                        <div class="flex-column">
-                            <h2 class="h2-blue text-center">Nossa história</h2>
+                        <div class="flex-column text-lg-start text-center">
+                            <h2 class="h2-blue text-center mb-2">Nossa história</h2>
                             <p class="p">
                             Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando 
                             um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum é simplesmente uma simulação de texto da 
@@ -21,13 +37,21 @@
                             Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor
                             desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
                             </p>
+                            <p class="p">
+                            Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando 
+                            um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum é simplesmente uma simulação de texto da 
+                            indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
+                            impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de 
+                            modelos de tipos. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 d-flex align-items-center valores">
+                <div class="col-lg-6 d-flex align-items-center valores pt-lg-0 pt-4">
                     <div class="d-flex justify-content-start align-items-center">
                         <div class="flex-column px-2">
-                            <div class="flex-column w-100 d-flex justify-content-center text-center">
+                            <div class="item">
+                                <div class="flex-column w-100 d-flex justify-content-center text-center">
                                     <div class="text-center">
                                         <img width="50px" height="50px" class="" src="<?= asset('images/icones/meta.png') ?>" alt="">
                                     </div>
@@ -35,40 +59,45 @@
                                         Missão
                                     </h2>
                                 </div>
-                                <p class="p text-center">
-                                Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor
-                                desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
-                                </p>
-                            <div class="flex-column w-100 d-flex justify-content-center text-center">
-                                    <div class="text-center">
-                                        <img width="50px" height="50px" class="" src="<?= asset('images/icones/visao.png') ?>" alt="">
-                                    </div>
-                                    <h2 class="h2-blue text-center">
-                                        Visão
-                                    </h2>
-                                </div>
-                                <p class="p text-center">
-                                Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor
-                                desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
-                                </p>
-                                <div class="flex-column w-100 d-flex justify-content-center text-center">
-                                    <div class="text-center">
-                                        <img width="50px" height="50px" class="" src="<?= asset('images/icones/cliente.png') ?>" alt="">
-                                    </div>
-                                    <h2 class="h2-blue text-center">
-                                        Valores
-                                    </h2>
-                                </div>
-                                <p class="p text-center">
+                                    <p class="p text-center">
                                     Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor
                                     desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
-                                </p>
+                                    </p>
+                                </div>
+                                <div class="item">
+                                    <div class="flex-column w-100 d-flex justify-content-center text-center">
+                                        <div class="text-center">
+                                            <img width="50px" height="50px" class="" src="<?= asset('images/icones/visao.png') ?>" alt="">
+                                        </div>
+                                        <h2 class="h2-blue text-center">
+                                            Visão
+                                        </h2>
+                                    </div>
+                                    <p class="p text-center">
+                                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor
+                                    desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
+                                    </p>
+                                </div>
+                                <div class="item">
+                                    <div class="flex-column w-100 d-flex justify-content-center text-center">
+                                        <div class="text-center">
+                                            <img width="50px" height="50px" class="" src="<?= asset('images/icones/cliente.png') ?>" alt="">
+                                        </div>
+                                        <h2 class="h2-blue text-center">
+                                            Valores
+                                        </h2>
+                                    </div>
+                                    <p class="p text-center">
+                                        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor
+                                        desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row p-0">
+            <div class="row p-0 d-none d-lg-block">
                 <div class="col-12 w-100 p-0">
                     <div class="swiper consultoria-swiper">
                         <div class="swiper-wrapper my-lg-4 my-4">
